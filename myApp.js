97a940964af4329cdea73584890a2bbf76000b48
Yolber90 <yolber19@gmail.com>
2024-04-28 14:55:31 +0000

    added .env

--- myApp.js
+++ myApp.js
@@ -3,8 +3,19 @@ let app = express();
 
 console.log("Hello World")
 
+// app.get("/", function(req, res){
+//     res.send("Hello Express")
+// });
 
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/views/index.html")
+});
 
+app.use("/public", express.static(__dirname + "/public") );
+
+app.get("/json", function(req, res){
+    res.json({"message": "Hello json"})
+})
 
 
 
